f91a3bb522702e22766e175277a0ed034baa681c
lunaboona <hyaluna@gmail.com>
2020-09-27 13:52:15 -0300

    Create Order.java

---
+++ src/main/java/com/company/Order.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.List;
+
+public class Order {
+
+    private final int id;
+    private List<Item> items;
+    private String note;
+
+    public Order(int id) {
+        this.id = id;
+    }
+
+    public void AddItem(Item item) {
+        this.items.add(item);
+    }
+    public void RemoveItem(int index) {
+        this.items.remove(index);
+    }
+
+    public List<Item> GetItems() {
+        return this.items;
+    }
+
+    public void SetNote(String note) {
+        this.note = note;
+    }
+    public String GetNote() {
+        return this.note;
+    }
+
+    public float GetTotal() {
+        return this.items.stream().map(item -> item.price).reduce((float) 0, Float::sum);
+    }
+
+    public int GetId() {
+        return this.id;
+    }
+
+    public boolean IsValid() {
+        return this.items.size() != 0;
+    }
+
+}
