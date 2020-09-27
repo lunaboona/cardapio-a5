1de38f753cd1c786e49c3702e76701eab852cf5b
lunaboona <hyaluna@gmail.com>
2020-09-27 13:44:30 -0300

    Update Item.java

--- src/main/java/com/company/Item.java
+++ src/main/java/com/company/Item.java
@@ -3,8 +3,10 @@ package com.company;
 public class Item {
     String name;
     float price;
+    String category;
 
-    public Item(String name, float price) {
+    public Item(String category, String name, float price) {
+        this.category = category;
         this.name = name;
         this.price = price;
     }
